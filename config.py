95b5e0c1b0644fa4e4b691aae1eeb3a567838f6d
khufiyajasuse <140427665+khufiyajasuse@users.noreply.github.com>
2024-07-24 18:53:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6371924437"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://dusra:sLwO6AhOgadMNHL5@dusra.r7gkttx.mongodb.net/?retryWrites=true&w=majority&appName=dusra
-DB_name=dusra")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://dusra:sLwO6AhOgadMNHL5@dusra.r7gkttx.mongodb.net/?retryWrites=true&w=majority&appName=dusra")
 DB_NAME = os.environ.get("DATABASE_NAME", "dusra")
 
 #force sub channel id, if you want enable force sub
